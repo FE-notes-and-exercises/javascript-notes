73421b993cc6d4d3710008a0dff05924b36a0949
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-07-27 11:52:19 -0600

    Add notes

---
+++ basics.js
@@ -0,0 +1,39 @@
+// String interpolation
+
+var lotteryNumbers = [12, 33, 51, 62, 74, 88];
+//Use interpolation or concatenation to print the following string: 
+// "The second number is...33!"
+  console.log('The second number is...${lotterNumbers[1]}!')
+
+
+// Conditional Statements
+
+function example() {
+  if () {
+    return ;
+  } else if () {
+    return ;
+  } else {
+    return ;
+  }
+}
+
+// Logical operators
+
+// ==  equals(comparison)
+// === strict equality comparison operator
+
+// &&   and
+// ||   or
+
+// >    greater than
+// <    less than 
+// <=   less than or equal to
+// >=   greater than or equal to
+
+
+// Functions
+
+function displayFunctionSkeleton(/* parameters go here if needed */) {
+  // statements go here
+}  
